35e587e24585a353d97f72d1d655d48dcec6ba66
Rishanthakumar <rishanthakumar@gmail.com>
2018-05-26 14:30:46 +0530

    Add post confirmation trigger and code refactor

---
+++ src/cognito/post-confimation.js
@@ -0,0 +1,27 @@
+import {DynamoDB} from 'aws-sdk';
+import {CognitoIdentityServiceProvider} from 'aws-sdk';
+
+import * as crypto from 'crypto';
+
+let cognitoidentityserviceprovider = new CognitoIdentityServiceProvider();
+let dynamoDB = new DynamoDB.DocumentClient();
+
+export const handler = (event, context) => {
+    let params = {
+        UserPoolId: process.env.USER_POOL_ID,
+        Username: event.request.userAttributes.email,
+    };
+
+    event.request.userAttributes.uuid = crypto.randomBytes(16).toString('hex');
+
+    dynamoDB.put({
+        TableName: process.env.ORGANIZATIONS_TABLE,
+        Item: event.request.userAttributes,
+      }, (err) => {
+        if (err) console.log(err);
+        cognitoidentityserviceprovider.adminDisableUser(params, (err, data) => {
+            if (err) context.done(null, event);
+            else context.done(null, event);
+        });
+      });
+};
