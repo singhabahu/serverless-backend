2b2f10127ba74ff63d9669096460088da679d7c3
Savidya Sathyani <savidya19@gmail.com>
2018-06-02 12:06:31 +0530

    Update the log function to accept a single object with the message as value and the type as the level

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -5,16 +5,17 @@ import packageJson from '../../package.json';
 class Logger {
     /**
      * Reusable log function, can be used to display a detailed logging info.
-     * @param {*} message
-     * @param {*} level - can be either ERROR, INFO or WARNING, Default is ERROR
+     * @param {*} message -
+     *              include a value and the level
+     *              level can be either ERROR, INFO or WARNING, Default one is ERROR
      */
-    static log(message, level = 'ERROR') {
+    static log(message) {
         let msg = {
-            level: level,
+            level: message.level,
             dateTime: new Date().toISOString(),
             version: packageJson.version,
             env: process.env.STAGE,
-            data: message,
+            data: message.value,
         };
         console.log(JSON.stringify(msg));
     }
