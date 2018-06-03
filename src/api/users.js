7750899c2a5955b6b17c90cbe54f245e40993b8a
Rishanthakumar <rishanthakumar@gmail.com>
2018-06-03 11:45:35 +0530

    Ability create new users under a super admin (#15)
    * Add user creation
    
    * Add reusable done method usage for user creation and error code correction
    
    * Add support for forgot password for new users
    
    * Add code refactor

--- src/api/users.js
+++ src/api/users.js
@@ -2,6 +2,7 @@
 import {User} from './../db/models/user';
 import * as Sequelize from 'sequelize';
 import Permission from '../util/permission';
+import {CognitoIdentityServiceProvider} from 'aws-sdk';
 import {done} from '../helpers/response-handler';
 
 /**
@@ -33,18 +34,130 @@ export const all = (event, context, callback) => {
             uuid: {[Sequelize.Op.ne]: uuid},
           },
         }).then((result) => {
-          result = {data: result};
-          return callback(null, done(null, result));
+          return callback(null, done(null, {
+            statusCode: 200,
+            data: result,
+          }));
         }).catch((error) => {
-          error.statusCode = 403;
-          return callback(null, done(error));
+          return callback(null, done({
+            statusCode: 500,
+            message: error,
+          }));
         });
       }).catch((error) => {
-        error.statusCode = 403;
-        return callback(null, done(error));
+        return callback(null, done({
+          statusCode: 500,
+          message: error,
+        }));
       });
     }).catch((error) => {
-      error.statusCode = 403;
-      return callback(null, done(error));
+      return callback(null, done({
+        statusCode: 500,
+        message: error.message,
+      }));
+    });
+};
+
+/**
+ * Create user under organization using cognito admin API.
+ * @param {*} event
+ * @param {*} context
+ * @param {*} callback
+ * @return {function} done
+ */
+export const create = (event, context, callback) => {
+  const cognito = new CognitoIdentityServiceProvider();
+
+  const name = JSON.parse(event.body).name;
+  const email = JSON.parse(event.body).email;
+  const roleId = JSON.parse(event.body).roleId;
+
+  if (
+    name == null || name.trim() == '' ||
+    email == null || email.trim() == '' ||
+    roleId == null || roleId == ''
+  ) {
+    return callback(null, done({
+      statusCode: 400,
+      message: `Request doesn't contain a valid object`,
+    }));
+  }
+
+  const uuid = event.requestContext.authorizer.principalId;
+  Permission.hasPermission(uuid, {realm: 'user', action: 'create'})
+    .then((confirmation) => {
+      if (!confirmation) {
+        return callback(null, done({
+          statusCode: 403,
+          message: `User doesn't have enough permission to perform this action`,
+        }));
+      };
+
+      const params = {
+        UserPoolId: process.env.USER_POOL_ID,
+        Username: email,
+        DesiredDeliveryMediums: [
+          'EMAIL',
+        ],
+        UserAttributes: [
+          {
+            Name: 'email',
+            Value: email,
+          },
+          {
+            Name: 'custom:manualConfirmation',
+            Value: 'confirmed',
+          },
+          {
+            Name: 'email_verified',
+            Value: 'true',
+          },
+        ],
+      };
+      cognito.adminCreateUser(params, (error, data) => {
+        if (error) {
+          return callback(null, done({
+            statusCode: 400,
+            message: error,
+          }));
+        } else {
+          User.find({
+            attributes: ['uuid', 'organizationId'],
+            where: {
+              uuid: uuid,
+            },
+          }).then((user) => {
+            User.create({
+              uuid: data.User.Attributes
+                .find((element) => element.Name === 'sub').Value,
+              name: name,
+              email: email,
+              organizationId: user.organizationId,
+              roleId: roleId,
+            }).then((result) => {
+              return callback(null, done(null, {
+                statusCode: 200,
+                data: result,
+              }));
+            }).catch((error) => {
+              return callback(null, done({
+                statusCode: 500,
+                message: error,
+              }));
+            });
+          }).catch((error) => {
+            return callback(null, done({
+              statusCode: 500,
+              message: error,
+            }));
+          });
+        }
+      });
+    }).catch((error) => {
+      console.log(error);
+      return callback(null, done({
+        statusCode: 500,
+        message: error.message,
+      }));
     });
 };
